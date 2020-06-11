6f2a256da6af1cf0fb6e39776c90b9edf2a2a0cb
rlalasrl <rlaalsrl0810@naver.com>
2020-06-11 14:41:03 +0900

    creating Query user resolvers

--- graphql/user.ts
+++ graphql/user.ts
@@ -1,8 +1,11 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { gql } from 'apollo-server-express';
 
 import User from '../database/entities/User.entities';
+import CustomError from '@Lib/customError';
 
-export const typeDef = gql `
+export const typeDef = gql`
   type User {
     id: String!
     email: String!
@@ -15,9 +18,44 @@ export const typeDef = gql `
     users: [User]!
   }
 
-  type Mutation
+  type Mutation {
+    
+  }
 `;
 
 export const resolvers = {
+  Query: {
+    user: async (_ :any, name: { name: User['name'] }) => {
+      const repositoy: Repository<User> = getRepository(User);
 
+      try {
+        const user: User = await repositoy.findOne({
+          where: {
+            name,
+          },
+        });
+
+        if(!user) {
+          new CustomError({ name: 'Database_Error' });
+        }
+        return user;
+
+      } catch(err) {
+        console.log(err);
+      }
+    },
+    users: async () => {
+      const repositoy: Repository<User> = getRepository(User);
+      
+      try {
+        const user: User[] = await repositoy.find()
+
+        return user;
+
+      } catch(err) {
+        console.log(err);
+        new CustomError({ name: 'Database_Error' });
+      }
+    }
+  }
 }
